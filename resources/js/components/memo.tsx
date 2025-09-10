3558ef10c0fdb1cc204b89f2ccf0faa88a2eaee6
apostroef <prithaliacardions@gmail.com>
2025-09-10 16:13:04 +0700

    fix: component

--- resources/js/components/memo.tsx
+++ resources/js/components/memo.tsx
@@ -7,8 +7,8 @@ interface Memo {
 export const Memo: React.FC<Memo> = ({ memotext, number, className }) => {
     return (
         <>
-            <div className="h-[8rem] w-[12rem] rounded-lg border-1 border-black/40 bg-[#FFF0CD] p-4 md:h-auto md:w-[8rem] lg:h-[12rem] lg:w-[12rem]">
-                <div className={`${className} flex flex-row items-start justify-center gap-x-2 lg:gap-x-2 lg:p-2`}>
+            <div className="h-[8rem] w-[12rem] rounded-lg border-1 border-black/40 bg-[#FFF0CD] p-4 md:h-auto md:w-[8rem] lg:h-auto lg:w-auto">
+                <div className={`${className} flex flex-row items-start justify-center gap-x-2 lg:gap-x-2`}>
                     <h1 className="text-[1rem] text-black lg:text-[1.5rem]">{number}</h1>
                     <p className="font-shine text-[1.7rem] lg:text-[2rem]">{memotext}</p>
                 </div>
--- resources/js/components/projects/navbar-rwfd.tsx
+++ resources/js/components/projects/navbar-rwfd.tsx
@@ -1,10 +1,10 @@
 export function NavbarRwfd() {
     return (
-        <div className="fixed top-0 w-auto right-0 left-0 z-50 h-[3.6rem] bg-[#fffbed] md:h-[8rem] overflow-x-hidden">
-            <div className="fixed flex h-[3.6rem] w-full flex-row lg:gap-x-2 justify-start overflow-hidden px-2 md:h-[8rem] md:justify-start md:gap-x-24 md:px-8">
+        <div className="fixed top-0 right-0 left-0 z-50 h-[3.6rem] w-auto overflow-x-hidden bg-[#fffbed] md:h-[5rem] lg:h-[8rem]">
+            <div className="fixed flex h-[3.6rem] w-full flex-row justify-start overflow-hidden px-2 md:h-[5rem] md:justify-start md:gap-x-24 md:px-8 lg:h-[8rem] lg:gap-x-2">
                 <a
                     href="/"
-                    className="group relative z-10 mt-[1rem] flex w-1/2 flex-col items-center justify-center transition-all duration-300 ease-in-out hover:z-25 hover:mt-[2.1rem] hover:font-bold md:mt-[4.3rem] md:w-1/3 lg:mt-[3.6rem]"
+                    className="group relative z-10 mt-[1rem] flex w-1/2 flex-col items-center justify-center transition-all duration-300 ease-in-out hover:z-25 hover:mt-[0.7rem] hover:font-bold md:mt-[2.3rem] md:w-1/3 lg:mt-[3.6rem] lg:hover:mt-[3rem]"
                 >
                     <h1 className="absolute top-2 left-1/8 z-10 font-creato text-black md:top-1/5 md:text-[1.3rem] lg:top-2/5 lg:text-[1.5rem]">
                         homepage.
@@ -15,9 +15,9 @@ export function NavbarRwfd() {
 
                 <a
                     href=""
-                    className="relative z-20 mt-[-1.8rem] -ml-35 flex h-[10rem] w-1/2 flex-col items-center justify-center transition-all duration-300 ease-in-out hover:mt-2 hover:font-bold md:mt-5 md:ml-[-50] md:w-1/3"
+                    className="relative z-20 mt-[-1.8rem] -ml-30 flex h-[10rem] w-1/2 flex-col items-center justify-center transition-all duration-300 ease-in-out hover:mt-[-2.1rem] hover:font-bold md:mt-[-1.3rem] md:ml-[-50] md:w-1/3 md:hover:mt-[-1.5rem] lg:mt-[1.3rem] hover:lg:mt-[0.9rem]"
                 >
-                    <h1 className="absolute top-[3rem] left-[12.5%] z-10 font-creato text-black md:top-2/5 md:left-[12.5%] md:text-[1.3rem] lg:top-2/5 lg:text-[1.5rem]">
+                    <h1 className="absolute top-[3.4rem] left-[8.5%] z-10 font-creato text-black md:top-2/5 md:left-[12.5%] md:text-[1.3rem] lg:top-11 lg:text-[1.5rem]">
                         projects/RWFD
                     </h1>
                     <img className="absolute inset-0 z-0 h-full w-full object-contain" src="/images/folder-plain.svg" />
