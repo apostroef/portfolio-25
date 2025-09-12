cf628f47ea43ef168c835a7115aea1bfaed31f2a
apostroef <prithaliacardions@gmail.com>
2025-09-12 14:56:13 +0700

    minor component fix

--- resources/js/components/projects/project-header.tsx
+++ resources/js/components/projects/project-header.tsx
@@ -9,15 +9,13 @@ interface ProjectHeader {
 export const ProjectHeader: React.FC<ProjectHeader> = ({ ProjectTitle, ProjectDesc, ProjectCover }) => {
     return (
         <>
-        <div className="flex flex-col items-center justify-center px-8 py-4 md:mt-24 md:px-32 lg:px-32 xl:px-64">
-                <div className="flex flex-col justify-center items-start md:items-center">
-                    <h1 className="font-creato text-[3rem] text-black md:text-[4rem] lg:text-[3.5rem] md:text-center">
-                        {ProjectTitle}
-                    </h1>
-                    <div className="flex flex-col items-start md:items-center justify-center">
-                        {ProjectDesc}
-                    </div>
+            <div className="flex flex-col items-center justify-center px-8 py-4 md:mt-4 md:px-32 lg:mt-16 lg:px-32 xl:px-64">
+                <div className="flex flex-col items-start justify-center md:items-center">
+                    <h1 className="font-creato text-[3rem] text-black md:text-center md:text-[4rem] lg:text-[3.5rem]">{ProjectTitle}</h1>
+                    <div className="flex flex-col items-start justify-center md:items-center">{ProjectDesc}</div>
                     <img src={ProjectCover} className="h-auto w-[36rem]" />
                 </div>
             </div>
-        </>)}
+        </>
+    );
+}
--- resources/js/components/projects/section.tsx
+++ resources/js/components/projects/section.tsx
@@ -5,13 +5,14 @@ SectionTitle: string;
 SectionBody: React.ReactNode;
 }
 export const ProjectSection : React.FC<ProjectSection> =({SectionTitle, SectionBody}) => {
-return (<>
-<div className="mt-4 items-start overflow-hidden">
-                    <div className="flex h-auto flex-col items-start justify-start">
-                        <h1 className="font-creato text-[1.5rem] font-bold text-black lg:text-[2rem]">{SectionTitle}</h1>
-                        <div className="flex flex-col items-start md:items-center justify-center">
-                            {SectionBody}
-                        </div>
-                    </div>
+    return (
+        <>
+            <div className="mt-4 items-start overflow-hidden">
+                <div className="flex h-auto w-full flex-col items-start justify-start">
+                    <h1 className="font-creato text-[1.5rem] font-bold text-black lg:text-[2rem]">{SectionTitle}</h1>
+                    <div className="flex flex-col items-start justify-center md:items-center">{SectionBody}</div>
                 </div>
-</>)}
+            </div>
+        </>
+    );
+}
