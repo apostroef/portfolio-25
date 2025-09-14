a8b3f2f68b867e3d47404c3a311bc115418b56d4
apostroef <prithaliacardions@gmail.com>
2025-09-14 16:18:49 +0700

    add component+img

--- resources/js/components/projects/section.tsx
+++ resources/js/components/projects/section.tsx
@@ -7,10 +7,10 @@ SectionBody: React.ReactNode;
 export const ProjectSection : React.FC<ProjectSection> =({SectionTitle, SectionBody}) => {
     return (
         <>
-            <div className="mt-4 items-start overflow-hidden">
-                <div className="flex h-auto w-full flex-col items-start justify-start">
+            <div className="mt-4 h-full w-full items-start overflow-hidden">
+                <div className="flex h-full w-full flex-col items-start justify-start">
                     <h1 className="font-creato text-[1.5rem] font-bold text-black lg:text-[2rem]">{SectionTitle}</h1>
-                    <div className="flex flex-col items-start justify-center md:items-center">{SectionBody}</div>
+                    <div className="flex h-full w-full flex-col md:items-center">{SectionBody}</div>
                 </div>
             </div>
         </>
