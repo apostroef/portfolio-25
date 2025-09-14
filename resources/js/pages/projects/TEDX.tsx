f435736dc6c87442ce40f594c88e18b983694742
apostroef <prithaliacardions@gmail.com>
2025-09-14 16:18:24 +0700

    add:tedx results

--- resources/js/pages/projects/TEDX.tsx
+++ resources/js/pages/projects/TEDX.tsx
@@ -46,11 +46,10 @@ export default function Rwfd() {
                             SectionBody={
                                 <>
                                     <p>
-                                        The internship was held for <span className="font-bold">3 months </span>with my friends,{' '}
-                                        <span className="font-bold">Arya & Hansen</span> with me as the team leader. I was given the responsibility
-                                        for <span className="bg-light-green font-bold">UI/UX and Front-End development</span> that includes
-                                        translating user needs into technical specifications, designing products using the Design Thinking framework,
-                                        creating responsive UI prototypes in Figma, and managing complete project documentation.
+                                        <span className="bg-light-green font-bold">TedX Universitas Airlangga 2024</span> was hosted under the Faculty
+                                        of Business. I was accepted by the Website Development Division as part of the UI/UX team, working closely
+                                        with the Front-End and Back-End teams to build a functional, user-friendly event website designed to handle
+                                        high visitor traffic.
                                     </p>{' '}
                                 </>
                             }
@@ -65,7 +64,7 @@ export default function Rwfd() {
                                                 <div className="relative rotate-[-5] py-2 transition-all duration-299 ease-in-out hover:rotate-1">
                                                     <Memo
                                                         number="1"
-                                                        memotext="How to fit all the themes?"
+                                                        memotext="How to design strong branding of TedXUA?"
                                                         className="h-auto w-[13rem] md:h-auto md:w-[10rem] lg:h-auto lg:w-auto"
                                                     />
                                                 </div>
@@ -73,6 +72,13 @@ export default function Rwfd() {
                                                     <Memo
                                                         number="2"
                                                         memotext="How to design efficient ticketing system?"
+                                                        className="h-auto w-[13rem] md:h-auto md:w-[10rem]"
+                                                    />
+                                                </div>
+                                                <div className="relative rotate-2 py-2 transition-all duration-299 ease-in-out hover:rotate-4">
+                                                    <Memo
+                                                        number="3"
+                                                        memotext="How to fit all the themes (3)?"
                                                         className="h-auto w-[13rem] md:h-auto md:w-[10rem]"
                                                     />
                                                 </div>
@@ -86,15 +92,48 @@ export default function Rwfd() {
                             SectionTitle="Results"
                             SectionBody={
                                 <>
-                                    <div className="overflow-hidden py-8">
-                                        <div className="flex flex-col items-center justify-start">
-                                            <img src="images/tedx/landing-page-pc.gif" className="h-full w-full object-contain lg:w-[35rem]" />
-                                            <p className="p-4 lg:p-8">
-                                                I was trusted to shape first impressions through design and interaction in{' '}
-                                                <span className="font-bold">Landing Page</span>. Consists of subtheme and event timeline that captures
-                                                general information regarding the event.
-                                            </p>
+                                    <div className="h-full w-full overflow-hidden py-8">
+                                        <div className="mt-8 rounded-2xl bg-black md:mt-2">
+                                            <div className="mt-8 flex h-full w-full flex-col rounded-2xl bg-black md:mt-2">
+                                                <div className="p-4">
+                                                    <h2 className="text-[1.5rem] font-bold !text-white">Landing Page</h2>
+                                                    <p className="!text-white">Design and showcase strong branding of TedXUA. </p>
+                                                </div>
+                                                <div className="grid grid-cols-1 items-end justify-center p-4 md:grid-cols-4 md:gap-12 md:p-8">
+                                                    <div className="col-span-3 flex flex-col items-center justify-center">
+                                                        <img
+                                                            src="images/tedx/landing-page-black.gif"
+                                                            className="h-full w-[25rem] md:w-[20rem] lg:w-[30rem]"
+                                                        />
+                                                        <p className="!text-white lg:p-2">Landing-Page</p>
+                                                    </div>{' '}
+                                                    <div className="mt-12 flex flex-col items-center justify-center md:mt-0">
+                                                        <img src="images/tedx/landing-phone.png" className="w-auto md:h-full lg:h-[17rem]" />
+                                                        <p className="text-center !text-white lg:p-2">Landing-Page-Phone</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="p-4">
+                                                <hr className="h-[0.05rem] w-full bg-white"></hr>
+                                                <h2 className="text-[1.5rem] font-bold !text-white">Landing Page</h2>
+                                                <p className="!text-white">Design and showcase strong branding of TedXUA. </p>
+                                            </div>
+                                            <div className="grid grid-cols-1 items-end justify-center p-4 md:grid-cols-4 md:gap-12 md:p-8">
+                                                <div className="col-span-3 flex flex-col items-center justify-center">
+                                                    <img
+                                                        src="images/tedx/landing-page-black.gif"
+                                                        className="h-full w-[25rem] md:w-[20rem] lg:w-[30rem]"
+                                                    />
+                                                    <p className="!text-white lg:p-2">Landing-Page</p>
+                                                </div>{' '}
+                                                <div className="mt-12 flex flex-col items-center justify-center md:mt-0">
+                                                    <img src="images/tedx/landing-phone.png" className="w-auto md:h-full lg:h-[17rem]" />
+                                                    <p className="text-center !text-white lg:p-2">Landing-Page-Phone</p>
+                                                </div>
+                                            </div>
                                         </div>
+
                                         <StickerPeel
                                             imageSrc="images/tedx/stiker-1.png"
                                             width={200}
@@ -104,18 +143,20 @@ export default function Rwfd() {
                                             peelDirection={40}
                                             shadowIntensity={0}
                                             lightingIntensity={0.01}
-                                            initialPosition={{ x: 100, y: 0 }}
+                                            initialPosition={{ x: 0, y: 0 }}
+                                            className="top-0 right-0 overflow-hidden"
                                         />
                                         <StickerPeel
                                             imageSrc="images/tedx/apple1.png"
-                                            width={200}
+                                            width={180}
                                             rotate={0}
                                             peelBackHoverPct={20}
                                             peelBackActivePct={40}
                                             peelDirection={40}
                                             shadowIntensity={0}
                                             lightingIntensity={0.01}
-                                            initialPosition={{ x: 100, y: 0 }}
+                                            initialPosition={{ x: 0, y: 0 }}
+                                            className="top-20 left-0 overflow-hidden"
                                         />
                                     </div>{' '}
                                 </>
