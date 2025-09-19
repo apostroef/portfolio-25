030b9e24c049004f177b952b4e2492a9caba0024
apostroef <prithaliacardions@gmail.com>
2025-09-19 11:40:58 +0700

    Updates

--- js/components/x-margin-line.tsx
+++
@@ -1,8 +0,0 @@
-export function XMargin() {
-    return (
-        <div>
-            <div className="fixed top-0 bottom-0 md:left-8 left-2 z-60 h-full border-l border-black"></div>
-            <div className="fixed top-0 md:right-8 right-2 bottom-0 z-60 h-full border-r border-black"></div>
-        </div>
-    );
-}
